21112f8a7374a46924b96b611450bd818d0e13d2
aidata <aidata@10.121.3.179>
2022-10-27 10:10:46 +0300

    ilkDizi

--- Arrays/src/arrays/Arrays.java
+++ Arrays/src/arrays/Arrays.java
@@ -3,6 +3,14 @@ package arrays;
 public class Arrays {
 
 	public static void main(String[] args) {
+		int [] array = new int[10];
+		System.out.printf("%s%8s%n","Index","Value");
+		for (int counter = 0; counter < array.length; counter++)
+		{
+			array[counter] = counter * counter;
+		}
+		for (int counter = 0; counter < array.length;counter ++)
+				System.out.printf("%5d%8d%n",counter, array[counter]);
 	
 	}
 
